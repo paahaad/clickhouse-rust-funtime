6a4d1a8b3fc1827704c9b6318b90f611587d1671
Parvat Raj <parvat.raj2@gmail.com>
2024-11-22 00:26:38 +0530

    added some fix for connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // step 1: create connection with clickhous
+    let conn = db::ClickhouseConnection::new();
+    let db_name = String::from("SOLANA"); 
+    conn.create_db(&db_name).await?;
+    conn.create_table(&db_name).await?;
+
+    Ok(())
 }
